91d562919890258ea8853d711e201c506467a500
Sayyid <srw125@scarletmail.rutgers.edu>
2019-11-05 10:34:08 -0500

    Aliens
    Aliens code for python project

---
+++ Aliens.py
@@ -0,0 +1,12 @@
+#Importing the csv file of the data of UFO sighting in all 50 states.The user would be presented a tab that has all 50 states and they would select the state they are in after they hit submit an 8 bit version of the omnitirx from the show Ben 10
+#would display they percentage of people who have seen aliens out of 100,000 people. The objective of this dictionary is to iterate through the set of information from the csv file that came from sateillitaeinternet.com
+# When you get to the home screen ask them if they belive in aliens if yes then they go to the next page if they pressed yes then it would say select a state.
+#print "Do you belive in aliens"
+#if yes then proceed to next page if no then exit page 
+#print "Welcome to the Mother Ship"
+import csv
+#above I imported the csv of the data for the amount of alien sisghting per 100,000 in each state.
+Aliens_State = {
+    "Alabama":22.26
+    "Alaska": 75.94
+# I'm not to sure if I would do  a dictionary to easily iterate through the code to return the amount of people that have had an alien incounter.

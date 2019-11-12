2c6d7a8392803d2ea024c1029fa91148776f65bc
Sayyid <srw125@scarletmail.rutgers.edu>
2019-11-12 10:07:41 -0500

    Update Aliens.py
    updated

--- Aliens.py
+++ Aliens.py
@@ -2,11 +2,13 @@
 #would display they percentage of people who have seen aliens out of 100,000 people. The objective of this dictionary is to iterate through the set of information from the csv file that came from sateillitaeinternet.com
 # When you get to the home screen ask them if they belive in aliens if yes then they go to the next page if they pressed yes then it would say select a state.
 #print "Do you belive in aliens"
-#if yes then proceed to next page if no then exit page 
+#if yes then proceed to next page if no then exit page
 #print "Welcome to the Mother Ship"
 import csv
 #above I imported the csv of the data for the amount of alien sisghting per 100,000 in each state.
-Aliens_State = {
-    "Alabama":22.26
-    "Alaska": 75.94
+with csv open('Sheet1.csv') as csvfile:
+    readCSV = csv.reader(csvfile,)
+    for row in reader:
+        print (row['State'], row['Sightings']
+
 # I'm not to sure if I would do  a dictionary to easily iterate through the code to return the amount of people that have had an alien incounter.
